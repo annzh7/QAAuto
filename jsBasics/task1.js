c19c53bafeae4f5987997e5d28c4bd5d56bbd2a1
AnnZhuravel <ann.zhuravel7@gmail.com>
2025-08-14 02:32:39 +0300

    added files with tasks 1-4

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+const user = "Anna";
+let age = 30;
+let isQA = true;
+let d;
+let c = null;
+
+console.log (user);
+console.log (age);
+console.log (isQA);
+console.log (d);
+console.log (c);
---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+let firstPerson = 'Alex';
+let secondPerson = 'Mary';
+
+let greeting1 = 'Dear ' + firstPerson + ' ' + 'and ' + secondPerson + '. ' + 'Congratulations with your achievements!';
+console.log (greeting1);
+
+let greeting2 = `Dear ${firstPerson} and ${secondPerson}. Congratulations with your achievements!`;
+console.log (greeting2);
