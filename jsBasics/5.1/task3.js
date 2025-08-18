1bdb390a63e9e8e33d88c6b06b24ddb13791e7e5
AnnZhuravel <ann.zhuravel7@gmail.com>
2025-08-18 20:36:18 +0300

    added new tasks

---
+++ jsBasics/5.1/task3.js
@@ -0,0 +1,13 @@
+const number = 5;
+
+for (let i = 1; i <= 10; i++) {
+    console.log (`${number} x ${i} =`, number*i )
+};
+
+console.log('\n');
+
+let count = 1;
+while (count <= 10) {
+    console.log(`${number} x ${count} =`, number*count);
+    count++;
+};
