5f49a53735066ba016d181b212f7f15ce51511d0
AnnZhuravel <ann.zhuravel7@gmail.com>
2025-09-01 18:22:20 +0300

    added tasks to lesson 8

---
+++ jsBasics/8.1/task2.js
@@ -0,0 +1,3 @@
+const numbers = [1, 2, 3, 4, 5];
+const newNumbers = numbers.map((value, index) => value * index);
+console.log (newNumbers);
---
+++ jsBasics/8.1/task5.js
@@ -0,0 +1,4 @@
+const firstArray = [1, 2];
+const secondArray = [3, 4];
+const newArray = firstArray.concat(secondArray);
+console.log (newArray);
