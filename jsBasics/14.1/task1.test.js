88edf175b8acc5c36cab78acdd77b42696aec1e3
Anna Zhuravel <ann.zhuravel7@gmail.com>
2025-10-30 00:48:39 +0200

    added interceptors

--- jsBasics/14.1/task1.test.js
+++ jsBasics/14.1/task1.test.js
@@ -1,65 +1,79 @@
 const axios = require('axios');
 
-const BASE_URL = 'https://jsonplaceholder.typicode.com';
+const api = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  headers: { 'Content-Type': 'application/json' },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    console.log('[REQUEST]', {
+      method: config.method?.toUpperCase(),
+      url: config.baseURL + config.url,
+      headers: config.headers,
+      data: config.data,
+    });
+    return config;
+  },
+  (error) => {
+    console.error('[REQUEST ERROR]', error.message);
+    return Promise.reject(error);
+  },
+);
+
+api.interceptors.response.use(
+  (response) => {
+    console.log('[RESPONSE]', {
+      status: response.status,
+      url: response.config.url,
+      data: response.data,
+    });
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      console.error('[RESPONSE ERROR]', {
+        status: error.response.status,
+        url: error.config.url,
+        data: error.response.data,
+      });
+    } else {
+      console.error('[NETWORK ERROR]', error.message);
+    }
+    return Promise.reject(error);
+  },
+);
 
 test('Get all posts', async () => {
-  const response = await axios.get(`${BASE_URL}/posts`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  console.log(response.data);
+  const response = await api.get('/posts');
   expect(response.status).toBe(200);
   expect(Array.isArray(response.data)).toBe(true);
 });
 
 test('Get all users', async () => {
-  const response2 = await axios.get(`${BASE_URL}/users`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  console.log(response2.data);
-  expect(response2.status).toBe(200);
-  expect(Array.isArray(response2.data)).toBe(true);
+  const response = await api.get('/users');
+  expect(response.status).toBe(200);
+  expect(Array.isArray(response.data)).toBe(true);
 });
 
 test('Get all albums', async () => {
-  const response3 = await axios.get(`${BASE_URL}/albums`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  console.log(response3.data);
-  expect(response3.status).toBe(200);
-  expect(Array.isArray(response3.data)).toBe(true);
+  const response = await api.get('/albums');
+  expect(response.status).toBe(200);
+  expect(Array.isArray(response.data)).toBe(true);
 });
 
 test('Get album with id 1', async () => {
-  const response4 = await axios.get(`${BASE_URL}/albums/1`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  console.log(response4.data);
-  expect(response4.status).toBe(200);
-  expect(response4.data).toHaveProperty('id', 1);
+  const response = await api.get('/albums/1');
+  expect(response.status).toBe(200);
+  expect(response.data).toHaveProperty('id', 1);
 });
 
 test('Add new post', async () => {
-  var response5 = await axios.post(
-    `${BASE_URL}/posts`,
-    {
-      title: 'foo',
-      body: 'bar',
-      userId: 1,
-    },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    },
-  );
-  console.log(response5.data);
-  expect(response5.data).toHaveProperty('id');
+  const response = await api.post('/posts', {
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  });
+  expect(response.status).toBe(201);
+  expect(response.data).toHaveProperty('id');
 });
